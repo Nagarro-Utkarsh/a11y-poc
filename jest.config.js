071145a16002756ded9de31c7c8f3508718288c8
Utkarsh Mahajan <utkarsh.mahajan@nagarro.com>
2025-04-07 18:46:51 +0530

    added jest

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,10 @@ export default {
     },
 
     moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '^.+\\.(jpg|ico|jpeg|png|gif|eot|svg|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            'jest-svg-transformer',
+        '\\.(css|less|sass|svg|scss)$': 'identity-obj-proxy',
+        '^.+\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            'jest-transformer-svg',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleDirectories: ['<rootDir>/src', 'node_modules'],
 };
-
