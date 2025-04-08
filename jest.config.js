6de63d50278615ff77484bdb02391348cf506f20
Utkarsh Mahajan <utkarsh.mahajan@nagarro.com>
2025-04-08 15:39:22 +0530

    fixed issue with svg imports in tests

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,10 @@ export default {
     },
 
     moduleNameMapper: {
-        '\\.(css|less|sass|svg|scss)$': 'identity-obj-proxy',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '^.+\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             'jest-transformer-svg',
+        '^.+\\.svg(\\?react)?$': 'jest-transformer-svg',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleDirectories: ['<rootDir>/src', 'node_modules'],
